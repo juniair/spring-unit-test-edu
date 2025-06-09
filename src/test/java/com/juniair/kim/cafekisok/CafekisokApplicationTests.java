be71ec3a58059c0fedb84172bc846eced812fc10
KimKyengJun <juniair.kim@gmail.com>
2025-06-09 21:46:41 +0900

    init

---
+++ src/test/java/com/juniair/kim/cafekisok/CafekisokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniair.kim.cafekisok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekisokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
